fa1047cb50234a3515191ce617b53b8008711ca6
Jonathan Lima <greenboxal@gmail.com>
2018-03-04 06:44:51 +0000

    Add websocket support

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -7,17 +7,19 @@ import (
 
 	"github.com/twz123/oidc-reverse-proxy/pkg/auth"
 	"github.com/twz123/oidc-reverse-proxy/pkg/sessions"
+	"github.com/yhat/wsutil"
 )
 
 type Upstream struct {
 	URL       *url.URL
-	Transport http.RoundTripper
+	Transport *http.Transport
 }
 
 func NewAuthProxyHandler(upstream *Upstream, flow auth.Flow, sessions sessions.Store, sessionCookieTemplate *http.Cookie) http.Handler {
 	handler := &handler{
 		flow:                  flow,
 		reverseProxy:          httputil.NewSingleHostReverseProxy(upstream.URL),
+		wsReverseProxy:        wsutil.NewSingleHostReverseProxy(toWebsocketURL(upstream.URL)),
 		sessions:              sessions,
 		sessionCookieTemplate: sessionCookieTemplate,
 	}
@@ -25,12 +27,19 @@ func NewAuthProxyHandler(upstream *Upstream, flow auth.Flow, sessions sessions.S
 	handler.reverseProxy.Transport = upstream.Transport
 	handler.reverseProxy.ModifyResponse = handler.modifyProxyResponse
 
+	if upstream.URL.Scheme == "https" {
+		handler.wsReverseProxy.Dial = upstream.Transport.DialTLS
+	} else {
+		handler.wsReverseProxy.Dial = upstream.Transport.Dial
+	}
+
 	return handler
 }
 
 type handler struct {
 	flow                  auth.Flow
 	reverseProxy          *httputil.ReverseProxy
+	wsReverseProxy        *wsutil.ReverseProxy
 	sessions              sessions.Store
 	sessionCookieTemplate *http.Cookie
 }
@@ -71,8 +80,13 @@ func (h *handler) serveSession(session sessions.LockedSession, request *http.Req
 
 	return func(responseWriter http.ResponseWriter) {
 		redirectURL := authentication.InjectInto(request)
+
 		if redirectURL == nil {
-			h.reverseProxy.ServeHTTP(responseWriter, request)
+			if wsutil.IsWebSocketRequest(request) {
+				h.wsReverseProxy.ServeHTTP(responseWriter, request)
+			} else {
+				h.reverseProxy.ServeHTTP(responseWriter, request)
+			}
 		} else {
 			http.Redirect(responseWriter, request, redirectURL.String(), http.StatusTemporaryRedirect)
 		}
@@ -87,4 +101,16 @@ func (h *handler) sessionID(request *http.Request) (string, error) {
 	}
 
 	return sessionCookie.Value, nil
+}
+
+func toWebsocketURL(original *url.URL) *url.URL {
+	result := *original
+
+	if result.Scheme == "https" {
+		result.Scheme = "wss"
+	} else {
+		result.Scheme = "ws"
+	}
+
+	return &result
 }
